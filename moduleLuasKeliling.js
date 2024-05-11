8df026392cd28a8a12fd98a57e8a47427b53f130
zakyy m <zakyym@gmail.com>
2024-05-11 21:40:02 +0700

    NodeJS

---
+++ moduleLuasKeliling.js
@@ -0,0 +1,15 @@
+exports.luasPersegi = function (x) {
+    return x * x;
+}
+
+exports.kelilingPersegi = function (x) {
+    return 4 * x ;
+}
+
+exports.luasPersegiPanjang = function (x,y) {
+    return x * y;
+}
+
+exports.kelilingPersegiPanjang = function (x,y) {
+    return 2 * (x + y);
+}
---
+++ soal1.js
@@ -0,0 +1,9 @@
+const calc = require('./moduleLuasKeliling.js')
+
+// Persegi
+console.log(calc.luasPersegi(5));
+console.log(calc.kelilingPersegi(5));
+
+// Persegi Panjang
+console.log(calc.luasPersegiPanjang(5,8));
+console.log(calc.kelilingPersegiPanjang(5,8));
